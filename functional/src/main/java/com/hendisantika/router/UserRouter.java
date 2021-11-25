3f8db98be253d7a410e09d885c991a5fc737223b
Hendi Santika <hendisantika@gmail.com>
2021-11-26 06:51:58 +0700

    #24 - Add delete in UserRouter

--- functional/src/main/java/com/hendisantika/router/UserRouter.java
+++ functional/src/main/java/com/hendisantika/router/UserRouter.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
@@ -41,5 +42,11 @@ public class UserRouter {
         return route(POST("/users")
                 .or(PUT("/users"))
                 .and(accept(MediaType.APPLICATION_JSON)), userHandler::save);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> delete(UserHandler userHandler) {
+        return route(DELETE("/users/{id}")
+                .and(accept(MediaType.APPLICATION_JSON)), userHandler::delete);
     }
 }
