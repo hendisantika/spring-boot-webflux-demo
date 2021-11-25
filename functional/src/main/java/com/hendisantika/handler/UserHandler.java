ccfb3c2a89edbfdbbddb11695b0145886a1ac775
Hendi Santika <hendisantika@gmail.com>
2021-11-26 06:47:02 +0700

    #15 - Add findAll in UserHandler

--- functional/src/main/java/com/hendisantika/handler/UserHandler.java
+++ functional/src/main/java/com/hendisantika/handler/UserHandler.java
@@ -4,9 +4,15 @@ import com.hendisantika.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 import javax.xml.validation.ValidatorHandler;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +33,14 @@ public class UserHandler {
 
     private final ValidatorHandler validatorHandler;
 
+    public Mono<ServerResponse> findAll(ServerRequest request) {
+        return userRepository.findAll()
+                .collectList()
+                .flatMap(users -> {
+                    if (users.isEmpty()) {
+                        return ServerResponse.noContent().build();
+                    }
+                    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(fromValue(users));
+                });
+    }
 }
