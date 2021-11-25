48dc0464937f3a4e8775311dad142cc64e035d99
Hendi Santika <hendisantika@gmail.com>
2021-11-26 06:51:07 +0700

    #21 - Add findAllRouter in UserRouter

--- functional/src/main/java/com/hendisantika/router/UserRouter.java
+++ functional/src/main/java/com/hendisantika/router/UserRouter.java
@@ -1,6 +1,15 @@
 package com.hendisantika.router;
 
+import com.hendisantika.handler.UserHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,10 +22,10 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class UserRouter {
-//    @Bean
-//    public RouterFunction<ServerResponse> findAllRouter(UserHandler userHandler) {
-//        return route(GET("/users")
-//                .and(accept(MediaType.APPLICATION_JSON)), userHandler::findAll);
-//    }
+    @Bean
+    public RouterFunction<ServerResponse> findAllRouter(UserHandler userHandler) {
+        return route(GET("/users")
+                .and(accept(MediaType.APPLICATION_JSON)), userHandler::findAll);
+    }
 
 }
