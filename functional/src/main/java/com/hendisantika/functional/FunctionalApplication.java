63d1d916cf29beeea9ef46499fa7099423feb5ec
Hendi Santika <hendisantika@gmail.com>
2021-11-26 06:22:23 +0700

    #2 - Add functional module

---
+++ functional/src/main/java/com/hendisantika/functional/FunctionalApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.functional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalApplication.class, args);
+    }
+
+}
---
+++ functional/src/test/java/com/hendisantika/functional/FunctionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.functional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
