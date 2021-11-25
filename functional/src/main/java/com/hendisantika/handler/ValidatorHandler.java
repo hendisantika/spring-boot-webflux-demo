c3db8f50647655205f607d253a389035e31e2ddc
Hendi Santika <hendisantika@gmail.com>
2021-11-26 06:48:47 +0700

    #19 - Add ValidatorHandler

---
+++ functional/src/main/java/com/hendisantika/handler/ValidatorHandler.java
@@ -0,0 +1,33 @@
+package com.hendisantika.handler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebInputException;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/11/21
+ * Time: 06.48
+ */
+@Component
+@RequiredArgsConstructor
+public class ValidatorHandler {
+
+    private final Validator validator;
+
+    public <T> void validate(T o) {
+        Set<ConstraintViolation<T>> validate = validator.validate(o);
+        if (!validate.isEmpty()) {
+            ConstraintViolation<T> violation = validate.stream().iterator().next();
+            throw new ServerWebInputException(violation.toString());
+        }
+    }
+}
