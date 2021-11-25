aec36b4d7eb9996af4633ce11e542aeb25d35cdd
Hendi Santika <hendisantika@gmail.com>
2021-11-26 06:41:15 +0700

    #7 - Add Main App SpringWebFluxApplication

---
+++ functional/src/main/java/com/hendisantika/SpringWebFluxApplication.java
@@ -0,0 +1,20 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/11/21
+ * Time: 06.40
+ */
+@SpringBootApplication
+public class SpringWebFluxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebFluxApplication.class);
+    }
+}
