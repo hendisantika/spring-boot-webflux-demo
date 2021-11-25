0437219e72984796db1d687033cff02fc49e86af
Hendi Santika <hendisantika@gmail.com>
2021-11-26 06:54:56 +0700

    #26 - Add findAllTest in SpringWebFluxApplicationTests

--- functional/src/test/java/com/hendisantika/SpringWebFluxApplicationTest.java
+++ functional/src/test/java/com/hendisantika/SpringWebFluxApplicationTest.java
@@ -1,11 +1,21 @@
 package com.hendisantika;
 
+import com.hendisantika.entity.User;
 import com.hendisantika.handler.UserHandler;
 import com.hendisantika.repository.UserRepository;
 import com.hendisantika.router.UserRouter;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,5 +38,24 @@ class SpringWebFluxApplicationTests {
     @MockBean
     private UserRepository userMongoRepository;
 
+    @Test
+    public void findAllTest() {
+        WebTestClient client = WebTestClient
+                .bindToRouterFunction(userRouter.findAllRouter(userHandler))
+                .build();
 
+        List<User> users = Arrays.asList(new User("Valentino", "Rossi"),
+                new User("Filippo", "Inzaghi"));
+
+        Flux<User> flux = Flux.fromIterable(users);
+        given(userMongoRepository.findAll())
+                .willReturn(flux);
+
+        client.get().uri("/users")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(User.class)
+                .isEqualTo(users);
+    }
 }
