aa5bd85b4907739da77fa4fcefeb1f01f7471c47
Hendi Santika <hendisantika@gmail.com>
2021-11-26 06:47:46 +0700

    #17 - Add save in UserHandler

--- functional/src/main/java/com/hendisantika/handler/UserHandler.java
+++ functional/src/main/java/com/hendisantika/handler/UserHandler.java
@@ -1,5 +1,6 @@
 package com.hendisantika.handler;
 
+import com.hendisantika.entity.User;
 import com.hendisantika.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
 import javax.xml.validation.ValidatorHandler;
@@ -49,4 +51,21 @@ public class UserHandler {
                 .flatMap(user -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(fromValue(user)))
                 .switchIfEmpty(ServerResponse.notFound().build());
     }
+
+    public Mono<ServerResponse> save(ServerRequest request) {
+        return request.bodyToMono(User.class)
+                .doOnNext(validatorHandler::validate)
+                .flatMap(userRepository::save)
+                .doOnSuccess(userSaved -> LOGGER.info("User saved with id: " + userSaved.getId()))
+                .doOnError(e -> LOGGER.error("Error in saveUser method", e))
+                .map(userSaved -> UriComponentsBuilder.fromPath(("/{id}")).buildAndExpand(userSaved.getId()).toUri())
+                .flatMap(uri -> ServerResponse.created(uri).build());
+//                .onErrorResume(throwable -> {
+//                    if(throwable instanceof ValidationException) {
+//                        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, throwable.getMessage());
+//                    }
+//                    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, throwable.getMessage());
+//                });
+    }
+
 }
