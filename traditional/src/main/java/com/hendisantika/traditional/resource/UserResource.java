0cd2ac092c57bae120855039048773bc02d6dec9
Hendi Santika <hendisantika@gmail.com>
2021-11-27 06:03:39 +0700

    #40 - Add findAll APA in UserResource in traditional module

--- traditional/src/main/java/com/hendisantika/traditional/resource/UserResource.java
+++ traditional/src/main/java/com/hendisantika/traditional/resource/UserResource.java
@@ -1,9 +1,15 @@
 package com.hendisantika.traditional.resource;
 
+import com.hendisantika.traditional.entity.User;
 import com.hendisantika.traditional.repository.UserMongoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +27,15 @@ public class UserResource {
 
     private final UserMongoRepository userMongoRepository;
 
+    @GetMapping
+    public Mono<ResponseEntity<List<User>>> findAll() {
+        return userMongoRepository.findAll()
+                .collectList()
+                .map(users -> {
+                    if (users.isEmpty()) {
+                        return ResponseEntity.noContent().build();
+                    }
+                    return ResponseEntity.ok(users);
+                });
+    }
 }
