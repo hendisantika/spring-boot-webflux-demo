c98d2b861bd3d5e3ba2fefcc26d25f551ae6a63e
Hendi Santika <hendisantika@gmail.com>
2021-11-27 05:58:24 +0700

    #33 - Add MongoConfig in traditional module

---
+++ traditional/src/main/java/com/hendisantika/traditional/config/MongoConfig.java
@@ -0,0 +1,25 @@
+package com.hendisantika.traditional.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/11/21
+ * Time: 05.57
+ */
+@Configuration
+@Profile("dev")
+@RequiredArgsConstructor
+public class MongoConfig extends AbstractReactiveMongoConfiguration {
+
+    private final Environment env;
+
+}
