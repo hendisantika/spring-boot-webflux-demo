e62017bcc992a39967c97f52871387e415f4ddfb
Hendi Santika <hendisantika@gmail.com>
2021-11-27 05:54:39 +0700

    #30 - Add traditional module

---
+++ traditional/src/main/java/com/hendisantika/traditional/TraditionalApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.traditional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraditionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraditionalApplication.class, args);
+    }
+
+}
---
+++ traditional/src/test/java/com/hendisantika/traditional/TraditionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.traditional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraditionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
