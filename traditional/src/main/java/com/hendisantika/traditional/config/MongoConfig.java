51af99fe78f1d8d85ec380158e996604ae7ad555
Hendi Santika <hendisantika@gmail.com>
2021-11-27 05:58:45 +0700

    #34 - Add getDatabaseName in MongoConfig in traditional module

--- traditional/src/main/java/com/hendisantika/traditional/config/MongoConfig.java
+++ traditional/src/main/java/com/hendisantika/traditional/config/MongoConfig.java
@@ -22,4 +22,8 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
 
     private final Environment env;
 
+    @Override
+    protected String getDatabaseName() {
+        return "users";
+    }
 }
