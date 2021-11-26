a88c6656aead542efe89ee8b015cfd9759e10069
Hendi Santika <hendisantika@gmail.com>
2021-11-27 06:39:32 +0700

    #45 - Remove embedded mongoDB traditional module

--- traditional/src/main/java/com/hendisantika/traditional/TraditionalApplication.java
+++ traditional/src/main/java/com/hendisantika/traditional/TraditionalApplication.java
@@ -1,7 +1,11 @@
 package com.hendisantika.traditional;
 
+import com.hendisantika.traditional.entity.User;
+import com.hendisantika.traditional.repository.UserMongoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TraditionalApplication {
@@ -10,4 +14,13 @@ public class TraditionalApplication {
         SpringApplication.run(TraditionalApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner insertData(UserMongoRepository userMongoRepository) {
+        return args -> {
+            userMongoRepository.save(new User("Uzumaki", "Naruto")).subscribe();
+            userMongoRepository.save(new User("Uchiha", "Sasuke")).subscribe();
+            userMongoRepository.save(new User("Haruno", "Sakura")).subscribe();
+            userMongoRepository.save(new User("Hatake", "Kakashi")).subscribe();
+        };
+    }
 }
