6b1c1449c0f018b6ee0b67f87e93a51de9395811
Hendi Santika <hendisantika@gmail.com>
2021-11-27 06:05:50 +0700

    #43 - Add delete API in UserResource in traditional module

--- traditional/src/main/java/com/hendisantika/traditional/resource/UserResource.java
+++ traditional/src/main/java/com/hendisantika/traditional/resource/UserResource.java
@@ -3,12 +3,15 @@ package com.hendisantika.traditional.resource;
 import com.hendisantika.traditional.entity.User;
 import com.hendisantika.traditional.repository.UserMongoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
@@ -56,5 +59,14 @@ public class UserResource {
         return userMongoRepository.save(user)
                 .map(userSaved -> UriComponentsBuilder.fromPath(("/{id}")).buildAndExpand(userSaved.getId()).toUri())
                 .map(uri -> ResponseEntity.created(uri).build());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable String id) {
+//        userMongoRepository.deleteById(id); //does not work because it is not part of a chain, so it is not
+//        subscribed by any method
+        //nothing happens if you don't subscribe
+        userMongoRepository.deleteById(id).subscribe();
     }
 }
